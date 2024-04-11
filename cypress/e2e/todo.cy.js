6723fcf37aa168b21cb72634c5f342c3f96e64b6
anatorres91 <anita.torres1991@gmail.com>
2024-04-11 23:39:32 +0200

    prueba cypress

--- cypress/e2e/todo.cy.js
+++ cypress/e2e/todo.cy.js
@@ -1,31 +1,52 @@
 describe('template spec', () => {
-    it('agregar tarea', () => {
+    it('crear tarea', () => {
         cy.visit('https://todomvc.com/examples/react/dist/#/')
         cy.get('[data-testid="text-input"]').type('tarea1{enter}')
-        cy.contains('tarea1')
+        cy.get('[data-testid="todo-item-label"]').contains('tarea1')
+    })
+
+    it('tarea completada', () => {
+        cy.visit('https://todomvc.com/examples/react/dist/#/')
         cy.get('[data-testid="text-input"]').type('tarea2{enter}')
-        cy.contains('tarea2')
-        cy.get(':nth-child(1) > .view > [data-testid="todo-item-toggle"]').click()
+        cy.get('[data-testid="todo-item-toggle"]').check()
+        cy.get('[data-testid="todo-item-toggle"]').first().check()
+    })
+
+    it('Desmarcar tarea', () => {
+        cy.visit('https://todomvc.com/examples/react/dist/#/')
         cy.get('[data-testid="text-input"]').type('tarea3{enter}')
-        cy.contains('tarea3')
-        cy.get(':nth-child(2) > .view > [data-testid="todo-item-toggle"]').check()
-        cy.get(':nth-child(2) > .view > [data-testid="todo-item-toggle"]').uncheck()
-        cy.get(':nth-child(3) > .view > [data-testid="todo-item-label"]').dblclick()
-        cy.get('.view > .input-container > [data-testid="text-input"]').clear().type('tarea4{enter}')
-        cy.contains('tarea4')
-        
+        cy.get('[data-testid="todo-item-toggle"]').check()
+        cy.get('[data-testid="todo-item-toggle"]').uncheck()
+        cy.get('[data-testid="todo-item-toggle"]').first().uncheck()
+   
+    })
 
+    it('Editar tarea', () => {
+        cy.visit('https://todomvc.com/examples/react/dist/#/')
+        cy.get('[data-testid="text-input"]').type('tarea4{enter}')
+        cy.get('[data-testid="todo-item-label"]').dblclick()
+        cy.get('.view > .input-container > [data-testid="text-input"]').clear().type('tarea5{enter}')
+        cy.get('[data-testid="todo-item-label"]').contains('tarea5')
         
+    })
+    it('Borrar tarea', () => {
+        cy.visit('https://todomvc.com/examples/react/dist/#/')
+        cy.get('[data-testid="text-input"]').type('tarea5{enter}')
+
+})
+
+it('Filtrar tareas', () => {
+    cy.visit('https://todomvc.com/examples/react/dist/#/')
+        cy.get('[data-testid="text-input"]').type('tarea1{enter}')
+        cy.get('[data-testid="text-input"]').type('tarea2{enter}')
+        cy.get('[data-testid="text-input"]').type('tarea3{enter}')
+        cy.get('[data-testid="text-input"]').type('tarea4{enter}')
+        cy.get('[data-testid="text-input"]').type('tarea5{enter}')
+        cy.get('[data-testid="footer-navigation"] > :nth-child(3) > a').click()
+        cy.get(':nth-child(2) > a').click()
+        cy.get(':nth-child(1) > a').click()
+     
     
-    
-
-        
-
-
-
-
-
-
 
     })
 })
